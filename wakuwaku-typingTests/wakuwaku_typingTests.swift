0b4e050fdde030870d9569217710f75275202afa
Nagase Yoshimasa <yoshimasa_nagase@ttsystems.com>
2026-04-29 21:18:41 +0900

    Initial Commit

---
+++ wakuwaku-typingTests/wakuwaku_typingTests.swift
@@ -0,0 +1,17 @@
+//
+//  wakuwaku_typingTests.swift
+//  wakuwaku-typingTests
+//
+//  Created by Yoshimasa Nagase on 2026/04/29.
+//
+
+import Testing
+@testable import wakuwaku_typing
+
+struct wakuwaku_typingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
